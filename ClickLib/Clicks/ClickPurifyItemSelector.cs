afdcd9211ab167d35d148813935757c2fcf3514d
maributt <maributtocks@gmail.com>
2022-09-30 23:17:28 +0200

    remove project reference bc im stupid
    ill add it back when i need it, this is just for the ease of building + not wanting to put up with vs's bullshit honestly

---
+++ ClickLib/Clicks/ClickPurifyItemSelector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using ClickLib.Attributes;
+using ClickLib.Bases;
+
+namespace ClickLib.Clicks;
+
+/// <summary>
+/// Addon PurifyItemSelector.
+/// </summary>
+public sealed unsafe class ClickPurifyItemSelector : ClickBase<ClickPurifyItemSelector>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ClickPurifyItemSelector"/> class.
+    /// </summary>
+    /// <param name="addon">Addon pointer.</param>
+    public ClickPurifyItemSelector(IntPtr addon = default)
+        : base("PurifyItemSelector", addon)
+    {
+    }
+
+    public static implicit operator ClickPurifyItemSelector(IntPtr addon) => new(addon);
+
+    /// <summary>
+    /// Instantiate this click using the given addon.
+    /// </summary>
+    /// <param name="addon">Addon to reference.</param>
+    /// <returns>A click instance.</returns>
+    public static ClickPurifyItemSelector Using(IntPtr addon) => new(addon);
+
+    /// <summary>
+    /// Click the item at a given index.
+    /// </summary>
+    /// <param name="index">Index to select.</param>
+    [ClickName("reduce")]
+    public void Click(int index = 0)
+        => this.FireCallback(12, index);
+
+    public void Quit()
+        => this.FireCallback(-1);
+}
---
+++ ClickLib/Clicks/ClickPurifyResult.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+using ClickLib.Bases;
+
+namespace ClickLib.Clicks;
+
+/// <summary>
+/// Addon PurifyResult.
+/// </summary>
+public sealed unsafe class ClickPurifyResult : ClickBase<ClickPurifyResult>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ClickPurifyResult"/> class.
+    /// </summary>
+    /// <param name="addon">Addon pointer.</param>
+    public ClickPurifyResult(IntPtr addon = default)
+        : base("PurifyItemSelector", addon)
+    {
+    }
+
+    public static implicit operator ClickPurifyResult(IntPtr addon) => new(addon);
+
+    /// <summary>
+    /// Instantiate this click using the given addon.
+    /// </summary>
+    /// <param name="addon">Addon to reference.</param>
+    /// <returns>A click instance.</returns>
+    public static ClickPurifyResult Using(IntPtr addon) => new(addon);
+
+    /// <summary>
+    /// Click the close button.
+    /// </summary>
+    public void Close() => this.FireCallback();
+
+    /*/// <summary>
+    /// Test Function.
+    /// </summary>
+    public void Close(params int[] test)
+    {
+        try
+        {
+            switch (test.Length)
+            {
+                case 1:
+                    this.FireCallback(test[0]);
+                    break;
+                case 2:
+                    this.FireCallback(test[0], test[1]);
+                    break;
+                case 3:
+                    this.FireCallback(test[0], test[1], test[2]);
+                    break;
+                case 4:
+                    this.FireCallback(test[0], test[1], test[2], test[3]);
+                    break;
+                default:
+                    this.FireCallback();
+                    break;
+            }
+
+        }
+        catch (Exception e)
+        {
+        }
+    }*/
+}
