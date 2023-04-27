2eb451faba08cc13a00d8ad5a1f15887e3eb41a9
maributt <maributtocks@gmail.com>
2023-04-27 10:06:31 +0200

    Create ClickJournalAccept.cs

---
+++ ClickLib/Clicks/ClickJournalAccept.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using ClickLib.Bases;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace ClickLib.Clicks;
+
+/// <summary>
+/// Addon JournalAccept.
+/// </summary>
+public unsafe class ClickJournalAccept : ClickBase<ClickJournalAccept, AtkUnitBase>
+{
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ClickJournalAccept"/> class.
+    /// </summary>
+    /// <param name="addon">Addon pointer.</param>
+    public ClickJournalAccept(IntPtr addon = default)
+        : base("JournalAccept", addon)
+    {
+    }
+
+    public static implicit operator ClickJournalAccept(IntPtr addon) => new(addon);
+
+    /// <summary>
+    /// Instantiate this click using the given addon.
+    /// </summary>
+    /// <param name="addon">Addon to reference.</param>
+    /// <returns>A click instance.</returns>
+    public static ClickJournalAccept Using(IntPtr addon) => new(addon);
+
+    /// <summary>
+    /// Click the Accept button.
+    /// </summary>
+    /// <param name="acceptButton">Accept button pointer.</param>
+    public void Accept(AtkComponentButton* acceptButton) => this.ClickAddonButton(acceptButton, 1);
+}
